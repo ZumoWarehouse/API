16202dde475bd6cd4a79348bcbe833b2e181b7e0
Anton Boyko <boyko.ant@live.com>
2017-09-23 12:19:04 +0300

    adjust table helpers

--- _helpers/table.js
+++ _helpers/table.js
@@ -1,13 +1,23 @@
 var tableHelpers = {};
 
 tableHelpers.limitToUserId = function(context) {
+    var userId = '';
+    if (!!context.user) {
+        userId = context.user.id || '';
+    }
+
     // WHERE ... AND userId = 42
-    context.query.where({ userId: context.user.id });
+    context.query.where({ userId: userId });
     return context.execute();
 };
 
 tableHelpers.setUserId = function(context) {
-    context.item.userId = context.user.id;
+    var userId = '';
+    if (!!context.user) {
+        userId = context.user.id || '';
+    }
+
+    context.item.userId = userId;
     return context.execute();
 };
 
